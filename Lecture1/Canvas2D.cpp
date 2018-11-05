6c28a2cdfb857466feacfd486f055687db4e5655
TBCpp <jmhong.2019@gmail.com>
2018-11-05 22:12:49 +0900

    backup tank example.

---
+++ Lecture1/Canvas2D.cpp
@@ -0,0 +1,77 @@
+#include "Canvas2D.h"
+
+namespace jm
+{
+	Canvas2D::Canvas2D(const std::string & _title, const int & _width, const int & _height, const bool & use_full_screen, const int & display_ix)
+		: width(_width), height(_height)
+	{
+		if (!glfwInit()) reportErrorAndExit(__FUNCTION__, "glfw initialization");
+
+		const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+		int num_monitors;
+		GLFWmonitor ** monitors = glfwGetMonitors(&num_monitors);
+
+		if (!use_full_screen) {
+			glfw_window = glfwCreateWindow(_width, _height, _title.c_str(), NULL, NULL);
+
+			// center window
+			int display_w, display_h;
+			glfwMakeContextCurrent(glfw_window);
+			glfwGetFramebufferSize(glfw_window, &display_w, &display_h);
+
+			if (num_monitors == 3) // find center display
+				glfwSetWindowPos(glfw_window, (mode->width - display_w) / 2 - mode->width, (mode->height - display_h) / 2);
+			else
+				glfwSetWindowPos(glfw_window, (mode->width - display_w) / 2, (mode->height - display_h) / 2);
+		}
+		else {
+			if (display_ix < num_monitors) // display_ix is valid
+				glfw_window = glfwCreateWindow(mode->width, mode->height, _title.c_str(), monitors[display_ix], NULL);
+			else
+				glfw_window = glfwCreateWindow(mode->width, mode->height, _title.c_str(), glfwGetPrimaryMonitor(), NULL);
+
+			// full screen resolution
+			width = mode->width;
+			height = mode->height;
+		}
+
+		if (!glfw_window) reportErrorAndExit(__FUNCTION__, "Window initialization");
+
+		glfwMakeContextCurrent(glfw_window);
+
+		// Initialize GLEW
+		glewExperimental = true; // Needed for core profile
+		if (glewInit() != GLEW_OK) reportErrorAndExit(__FUNCTION__, "glew initialization");
+
+		const float aspect_ratio = (float)width / (float)height;
+		glViewport(0, 0, width, height);
+		glOrtho(-aspect_ratio, aspect_ratio, -1.0, 1.0, -1.0, 1.0);
+		std::cout << "Display width = " << width << " height = " << height <<
+			" Aspect ratio is " << aspect_ratio << std::endl;
+
+		// anti-aliasing
+		glfwWindowHint(GLFW_SAMPLES, 32);
+		glEnable(GL_LINE_SMOOTH);
+		glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
+		glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+		glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	}
+	
+	Canvas2D::~Canvas2D()
+	{
+		glfwDestroyWindow(glfw_window);
+		// 'delete glfw_window' doesn't work
+	}
+	
+	void Canvas2D::reportErrorAndExit(const std::string & function_name, const std::string & message)
+	{
+		std::cout << "Error: " << function_name << " " << message << std::endl;
+
+		glfwTerminate();
+		getchar(); // pause to read error message
+		exit(1);
+	}
+}
---
+++ Lecture1/RGBColors.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "RGB.h"
+
+namespace jm
+{
+	namespace RGBColors
+	{
+		// RGB color table http://www.rapidtables.com/web/color/RGB_Color.htm
+		const RGB red		(255, 0, 0);
+		const RGB green		(0, 255, 0);
+		const RGB blue		(0, 0, 255);
+		const RGB gray		(128, 128, 128);
+		const RGB yellow	(255, 255, 0);
+		const RGB olive		(128, 128, 0);
+		const RGB black		(0, 0, 0);
+		const RGB white		(1, 1, 1);
+	}
+}
