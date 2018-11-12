15e14c343208271cdf0eeb8424b756d40f7ff4d0
TBCpp <jmhong.2019@gmail.com>
2018-11-12 18:50:28 +0900

    Chapter 2, Lecture 1 draft.

---
+++ Chapter 2/Lecture1/Engine/RGB.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Vector3.h"
+
+namespace jm
+{
+	class RGB : public Vector3<float>
+	{
+		using BASE = Vector3<float>;
+
+	public:
+		RGB(const float & fr, const float & fg, const float & fb)
+			: BASE(fr, fg, fb)
+		{}
+
+		// integer type rgb values are divided by 255.0f because OpenGL uses real rgb values.
+		RGB(const int & cr, const int & cg, const int & cb)
+			: BASE(static_cast<float>(cr) / 255.0f, static_cast<float>(cg) / 255.0f, static_cast<float>(cb) / 255.0f)
+		{}
+
+		RGB(const RGB & _rgb)
+			: BASE(_rgb.r, _rgb.g, _rgb.b)
+		{}
+
+		~RGB()
+		{
+		}
+	};
+}
