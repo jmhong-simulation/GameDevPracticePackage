4ade9a70b400f600fce3d6fbdfac328de9776947
TBCpp <jmhong.2019@gmail.com>
2018-11-13 23:58:20 +0900

    add new

---
+++ Chapter 2/Lecture1/Engine/RandomNumberGenerator.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+#include <ctime>
+
+namespace jm
+{
+	class RandomNumberGenerator
+	{
+	public:
+		std::mt19937 mersenne;
+
+		RandomNumberGenerator()
+			: mersenne(static_cast<unsigned int>(std::time(nullptr)))
+		{
+		}
+
+		~RandomNumberGenerator()
+		{
+		}
+
+		int getInt(const int& min, const int& max)
+		{
+			return std::uniform_int_distribution<>(min, max)(mersenne);
+		}
+
+		float getFloat(const float& min, const float& max)
+		{
+			return static_cast<float>(std::uniform_real_distribution<>(min, max)(mersenne));
+		}
+
+		double getDouble(const double& min, const double& max)
+		{
+			return std::uniform_real_distribution<>(min, max)(mersenne);
+		}
+	};
+}
