263c5bb32093ee80534e137ea6d472af14a8362c
Mona Lisa <jmhong.2019@gmail.com>
2018-11-19 22:49:46 +0900

    Lecture 4 final

---
+++ Chapter 2/Lecture4_final/Box.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "GeometricObject.h"
+
+namespace jm
+{
+	class Box : public GeometricObject
+	{
+	public:
+		float width, height;
+
+		Box(const RGB & _color, const vec2 & _pos, const float & _width,
+			const float &_height)
+		{
+			init(_color, _pos, _width, _height);
+		}
+
+		void init(const RGB & _color, const vec2 & _pos, const float & _width, 
+			const float &_height)
+		{
+			GeometricObject::init(_color, _pos);
+
+			width = _width;
+			height = _height;
+		}
+
+		void drawGeometry() const override
+		{
+			drawFilledBox(GeometricObject::color, this->width, this->height);
+		}
+	};
+}
