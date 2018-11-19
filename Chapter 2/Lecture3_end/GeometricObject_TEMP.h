71702a2bb9c6982a1777e0cf96acfa52c2262b99
Mona Lisa <jmhong.2019@gmail.com>
2018-11-19 18:26:19 +0900

    Lecture 3 end.

---
+++ Chapter 2/Lecture3_end/GeometricObject_TEMP.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include "Game2D.h"
+
+namespace jm
+{
+	// this class shows what happens if you do not use OOP properly
+	class GeometricObject_TEMP
+	{
+	public:
+		enum TYPE
+		{
+			TRIANGLE, CIRCLE, BOX, // add more types
+		} type;
+
+		// common properties
+		vec2 pos;
+		RGB  color;
+
+		// for triangle and circle
+		float size;
+
+		// for box
+		float width;
+		float height;
+
+		void initTriangle(const RGB & _color, const vec2 & _pos,
+						  const float & _size)
+		{
+			color = _color;
+			pos = _pos;
+			size = _size;
+		}
+
+		void initCircle(const RGB & _color, const vec2 & _pos,
+						const float & _size)
+		{
+			color = _color;
+			pos = _pos;
+			size = _size;
+		}
+
+		void initBox(const RGB & _color, const vec2 & _pos, 
+					 const float & _width, const float & _height)
+		{
+			color = _color;
+			pos = _pos;
+			//size = _size; //Warning: do not use for Boxes
+			width = _width;
+			height = _height;
+		}
+
+		void draw()
+		{
+			if (type == TRIANGLE) {
+				beginTransformation();
+				{
+					translate(pos);
+					//drawWiredTriangle(Colors::black, size);
+					drawFilledTriangle(color, size);
+				}
+				endTransformation();
+			}
+			else if (type == CIRCLE)
+			{
+				beginTransformation();
+				{
+					translate(pos);
+					drawFilledCircle(color, size);
+				}
+				endTransformation();
+			}
+			else if (type == BOX)
+			{
+				beginTransformation();
+				{
+					translate(pos);
+					drawFilledBox(Colors::blue, this->width, this->height);
+				}
+				endTransformation();
+			}
+		}
+
+		/*
+		void drawGeometry()
+		{
+			if (type == TRIANGLE)
+				drawFilledTriangle(color, size);
+			else if (type == CIRCLE)
+				drawFilledCircle(color, size);
+			else if (type == BOX)
+				drawFilledBox(Colors::blue, this->width, this->height);
+			else
+				exit(-1);
+		}
+
+		void draw()
+		{
+			beginTransformation();
+			{
+				translate(pos);
+				drawGeometry();
+			}
+			endTransformation();
+		}
+		*/
+	};
+}
---
+++ Chapter 2/Lecture3_end/Triangle.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "GeometricObject.h"
+
+namespace jm
+{
+	class Triangle : public GeometricObject
+	{
+	public:
+		float size;
+
+		Triangle(const RGB & _color, const vec2 & _pos, const float & _size)
+		{
+			init(_color, _pos, _size);
+		}
+
+		~Triangle()
+		{
+			printf("Triangle destructor\n");
+		}
+
+		void init(const RGB & _color, const vec2 & _pos, const float & _size)
+		{
+			GeometricObject::init(_color, _pos);
+			size = _size;
+		}
+
+		void drawGeometry() const override
+		{
+			setLineWidth(3);
+			drawWiredTriangle(Colors::black, size);
+			drawFilledTriangle(color, size);
+		}
+	};
+}
